c00eb42205da6e8f872c6b4fb5e9f2ea03b4cdb7
brockar <martinnguzman.mg@gmail.com>
2024-08-07 00:59:15 -0300

    qmk

---
+++ firmware/config.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define SPLIT_HAND_PIN D2
+#define MASTER_LEFT
--- firmware/keymaps/default/config.h
+++ firmware/keymaps/default/config.h
@@ -2,7 +2,3 @@
 
 #pragma once
 
-#define VIAL_KEYBOARD_UID {0xAA, 0x95, 0x3C, 0x15, 0x34, 0xC6, 0x10, 0x2A}
-
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
