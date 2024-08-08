3812e5849b20edd8aa6aaee329681eb355bcd73c
brockar <martinnguzman.mg@gmail.com>
2024-08-08 00:53:22 -0300

    right doesnt work

--- firmware/keymaps/vial/config.h
+++ firmware/keymaps/vial/config.h
@@ -5,5 +5,5 @@
 //#define VIAL_KEYBOARD_UID {0xAA, 0x95, 0x3C, 0x15, 0x34, 0xC6, 0x10, 0x2A}
 #define VIAL_KEYBOARD_UID {0x4C, 0x83, 0x0B, 0x33, 0xB6, 0x24, 0xDF, 0x0A}
 
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
