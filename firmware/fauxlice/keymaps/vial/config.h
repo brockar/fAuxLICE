d5ef05618aef833ecd2ea57456e180da3bb50d36
brockar <martinnguzman.mg@gmail.com>
2024-08-08 18:55:46 -0300

    08/08

---
+++ firmware/fauxlice/keymaps/vial/config.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x4D, 0xA9, 0x09, 0xA3, 0xD3, 0x57, 0xD4, 0xAA}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {1, 2}
+
+#define MASTER_LEFT
+#define USE_SERIAL
