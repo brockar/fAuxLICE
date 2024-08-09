2dd2476e939e4eca4c9985e6c56b43185746b275
brockar <martinnguzman.mg@gmail.com>
2024-08-08 21:11:07 -0300

    a

---
+++ firmware/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+
+/* This file is empty and unrequired */
+
+#define USE_SERIAL
+#define MASTER_LEFT
--- firmware/keymaps/vial/config.h
+++ firmware/keymaps/vial/config.h
@@ -4,8 +4,6 @@
 
 #define VIAL_KEYBOARD_UID {0x4D, 0xA9, 0x09, 0xA3, 0xD3, 0x57, 0xD4, 0xAA}
 
-#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
-#define VIAL_UNLOCK_COMBO_COLS {1, 2}
+// #define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+// #define VIAL_UNLOCK_COMBO_COLS {1, 2}
 
-#define MASTER_LEFT
-#define USE_SERIAL
